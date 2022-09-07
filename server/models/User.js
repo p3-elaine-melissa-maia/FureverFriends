cc57f68c58ad9bafbd37da00b40781f02ecdc72a
Elaine Danahy <edanahy22@gmail.com>
2022-09-06 20:46:57 -0500

    added post and comment models

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const bcrypt = require('bcrypt');
 
@@ -47,11 +48,26 @@ friends: [
   id: false,
 });
 
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 userSchema.virtual('friendCount').get(function () {
   return `${this.friends.length}`;
 });
 
 // create the User model using the UserSchema
-const User = model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 // export the User model
 module.exports = User;
