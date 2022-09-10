c4844de06f16efcbe2cb629a7c0717fe79350466
ethantor12 <ethantor12@gmail.com>
2022-09-10 10:56:38 -0500

    added api fetch calls

---
+++ client/api/oauth-token.js
@@ -0,0 +1,20 @@
+// keys to use Petfinder API
+const petFinderKey = "85M0Le5ywilSAxXmQk5cvNWgrO03XluwDzz2ALbvGHwFQpSk3M";
+const petFinderSecret = "y6uvIhNy9I6se6Fu9zCRaHY8jf99I2hYSgczYJBg";
+
+// fetches and tracks the use of the keys
+export default async (req, res) => {
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
+    params.append("client_id", petFinderKey);
+    params.append("client_secret", petFinderSecret);
+    const petfinderRes = await fetch(
+        "https://api.petfinder.com/v2/oauth2/token",
+        {
+            method: "POST",
+            body: params
+        }
+    );
+    const data = await petfinderRes.json();
+    res.send(data);
+};
---
+++ client/api/petFinder.js
@@ -0,0 +1,25 @@
+import { useEffect, createContext, useState } from "react";
+
+const AuthContext = createContext();
+
+// provides application access to Petfinder API
+function petFinderApp ({ Component, pageProps }) {
+    const [accessToken, setAccessToken] = useState(null);
+
+    useEffect (() => {
+        const fetchAccessToken = async () => {
+            const res = await fetch("/api/oauth-token");
+            const json = await res.json();
+            setAccessToken(json.access_token);
+        };
+        fetchAccessToken();
+    }, []);
+
+    return (
+        <AuthContext.Provider>
+            <Component {...pageProps} />
+        </AuthContext.Provider>
+    );
+}
+
+export default petFinderApp;
