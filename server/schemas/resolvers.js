b2b79865bb0cc2463f83c045b1d5680fbaae050c
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-09-11 18:24:25 -0500

    added stripe files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,9 @@
 const { User, Comment, Post } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('pk_test_51LgrUmCwzDCX8biEBVfXXeZWz9TwWi5yG2q9k7lVBhctG9y2HCrcy4AVcltXn5VKD3dbEcxNTOgzVggc1lekY2tJ00sHgXbKu8');
+
+
 
 const resolvers = {
   Query: {
@@ -17,7 +20,45 @@ const resolvers = {
     },
     comments: async () => {
       return await Comment.find({});
+    },
+
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ products: args.products });
+      const line_items = [];
+
+      const { products } = await order.populate('products');
+
+      for (let i = 0; i < products.length; i++) {
+        const product = await stripe.products.create({
+          name: products[i].name,
+          description: products[i].description,
+          images: [`${url}/images/${products[i].image}`]
+        });
+
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: 'usd',
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+
+      return { session: session.id };
     }
+
   },
   // define functions that will fulfill the mutations
   Mutation: {
