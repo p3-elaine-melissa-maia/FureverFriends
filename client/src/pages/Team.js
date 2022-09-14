083a5943f916545aa5b4bf6a902ffce9a00ffb43
maiavelli <maiacamilledavis@gmail.com>
2022-09-13 20:01:47 -0400

    repushing team page

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,28 +1,38 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 // import { useQuery } from '@apollo/client';
-import '../styles/Home.css'
+import '../styles/Team.css'
+import placeholder from '../assets/placeholder.png'
 import { Link } from "react-router-dom";
 
 
 const Team = () => {
-  // const { loading, data } = useQuery(QUERY_MATCHUPS, {
-  //   fetchPolicy: "no-cache"
-  // });
-
-  // const matchupList = data?.matchups || [];
-
   return (
     <section>
-      {/* <Navbar></Navbar> */}
-      <div className="content">
-        <div className="info">
-            <h2> Find Your <br/> <span>Furever Friend <i className="fa-solid fa-heart"></i></span></h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laboriosam illum ratione quas iste inventore provident laborum, voluptate, reiciendis voluptas officiis distinctio doloribus magni, reprehenderit tempora. Id, molestiae. Ipsam, consequatur.</p>
-            <Link to="/signup" href="./signup" className="signup-btn">Sign Up Now!</Link>
-            <Link to="/login" href="./login" className="login-btn">Log In</Link>
-        </div>
-    </div>
+      <div className="team">
+                <div>
+                    <h2> Our Team </h2>
+                    <div className="teammember">
+                        <h3><img src={placeholder}/> <br/> Elaine </h3> 
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laboriosam illum ratione quas iste inventore provident laborum, voluptate, reiciendis voluptas officiis distinctio doloribus magni, reprehenderit tempora. Id, molestiae. Ipsam, consequatur.</p>
+            
+                    <div className="teammember">
+                        <h3><img src={placeholder}/> <br/> Ethan </h3> 
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laboriosam illum ratione quas iste inventore provident laborum, voluptate, reiciendis voluptas officiis distinctio doloribus magni, reprehenderit tempora. Id, molestiae. Ipsam, consequatur.</p>
+                    
+                    <div className="teammember">
+                        <h3><img src={placeholder}/> <br/> Maia </h3> 
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laboriosam illum ratione quas iste inventore provident laborum, voluptate, reiciendis voluptas officiis distinctio doloribus magni, reprehenderit tempora. Id, molestiae. Ipsam, consequatur.</p>
+                    
+                    <div className="teammember">
+                        <h3><img src={placeholder}/> <br/> Melissa </h3> 
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laboriosam illum ratione quas iste inventore provident laborum, voluptate, reiciendis voluptas officiis distinctio doloribus magni, reprehenderit tempora. Id, molestiae. Ipsam, consequatur.</p>
+                </div>
+            </div>
   </section>
   );
 };
