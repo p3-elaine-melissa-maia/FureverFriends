b922db10efb6713165c826fdbf37b637a3f30892
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-09-14 21:08:39 -0500

    added some team info and main page details

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
 // import { Link } from 'react-router-dom';
 // import { useQuery } from '@apollo/client';
-import '../styles/Home.css'
+import "../styles/Home.css";
 import { Link } from "react-router-dom";
-
 
 const Home = () => {
   // const { loading, data } = useQuery(QUERY_MATCHUPS, {
@@ -17,13 +16,27 @@ const Home = () => {
       {/* <Navbar></Navbar> */}
       <div className="content">
         <div className="info">
-            <h2> Find Your <br/> <span>Furever Friend <i className="fa-solid fa-heart"></i></span></h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laboriosam illum ratione quas iste inventore provident laborum, voluptate, reiciendis voluptas officiis distinctio doloribus magni, reprehenderit tempora. Id, molestiae. Ipsam, consequatur.</p>
-            <Link to="/signup" href="./signup" className="signup-btn">sign up now!</Link>
-            <Link to="/login" href="./login" className="login-btn">log in</Link>
+          <h2>
+            {" "}
+            Find Your <br />{" "}
+            <span>
+              Furever Friend <i className="fa-solid fa-heart"></i>
+            </span>
+          </h2>
+          <p>
+            This application is an accessible pet social media app where users
+            can search for adoptable pets, friend
+            other pet owners, and comment and like photos.
+          </p>
+          <Link to="/signup" href="./signup" className="signup-btn">
+            sign up now!
+          </Link>
+          <Link to="/login" href="./login" className="login-btn">
+            log in
+          </Link>
         </div>
-    </div>
-  </section>
+      </div>
+    </section>
   );
 };
 
